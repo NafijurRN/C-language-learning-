953092f8e1bf97ea5d5689fc5b1e61d01898d977
Md Nafijur Rahman Nasrat <nafijurrna@gmail.com>
2025-07-13 22:16:50 +0600

    Add files via upload

---
+++ conditional.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main ()
+{
+    int tk ;
+     printf("Amr kase koto tk ase :");
+    scanf("%d",&tk);
+    //printf("Amr kase koto tk ase :");
+    if(tk >= 100)
+    {
+        printf("Burger Khabo");
+
+    }
+    else
+
+    {
+        printf("Burger khabo na ");
+    }
+    return 0;
+}
