9283e598a44554b9e9cafd83cf82b4908fd8faea
Md Nafijur Rahman Nasrat <nafijurrna@gmail.com>
2025-07-13 22:14:41 +0600

    Add files via upload

---
+++ arithmatic opera.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int a = 10,b =20;
+
+    int sum=a+b;
+    printf("%d",sum);
+
+     int mns=a-b;
+    printf("\n%d",mns);
+
+    int mlt = a * b;
+    printf("\n%d",mlt);
+
+    int div = b/a;
+    printf("\n%d",div);
+
+
+     int c = 3;
+
+     int divi = b/c;
+     printf("\n%d",divi);
+
+     int mod= b%c;
+     printf("\n%d",mod);
+
+
+
+
+
+
+    return 0;
+}
