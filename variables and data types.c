fcbbd81ceb6c42a60fdf0b9b9faba8f767eb71e4
Md Nafijur Rahman Nasrat <nafijurrna@gmail.com>
2025-07-13 22:15:58 +0600

    Add files via upload

---
+++ variables and data types.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+
+
+    int rahim = 100,karim = 200;
+    printf("%d %d", karim, rahim);
+
+
+  float chol = 2.553;
+  printf("\n%0.3f",chol);
+
+  char ami = 'a';
+  printf("\n%c", ami);
+
+return 0;
+
+}
