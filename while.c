ef42ac3c1c9f464b828e10c13825ae9054bc8db7
Md Nafijur Rahman Nasrat <nafijurrna@gmail.com>
2025-07-15 00:07:46 +0600

    Add files via upload

---
+++ while.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    //while
+    i = 1; //initialization part
+    while(i<= 5) //condition part
+    {
+        printf("%d\n",i);
+        i= i+1;   //increment or decrement part
+    }
+
+
+
+
+    return 0;
+}
