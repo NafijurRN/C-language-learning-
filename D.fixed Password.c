276d04de45cb43628580e7cab6378f00893dc333
Md Nafijur Rahman Nasrat <nafijurrna@gmail.com>
2025-08-10 23:49:43 +0600

    Add files via upload

---
+++ D.fixed Password.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+int n ;
+  while(scanf("%d",&n)!=EOF)
+{
+   if (n==1999)
+   {
+   printf("Correct\n");
+   break;
+   }
+   else
+   {
+   printf("Worng\n");
+   }
+}
+    return 0;
+}
